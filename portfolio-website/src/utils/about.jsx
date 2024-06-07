3acd70d81d39d2e8a833db9e75dd8eaca2b36644
samuelsibarani11 <samuelalbi09@gmail.com>
2024-06-07 17:17:05 +0700

    first commit

---
+++ portfolio-website/src/utils/about.jsx
@@ -0,0 +1,24 @@
+/* eslint-disable react/no-unescaped-entities */
+// eslint-disable-next-line no-unused-vars
+import React from 'react';
+
+const About = () => {
+    return (
+        <>
+            <div className='bg-gray-900 text-white py-12'>
+                <div className='max-w-screen-xl mx-auto p-4 flex flex-col items-center md:space-x-10'>
+                    <div className="text-center md:text-left">
+                        <h1 className="text-5xl font-bold mb-4">
+                        About Me
+                        </h1>
+                        <h1>
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi iure vero ullam! Fugit consequuntur accusantium sit molestiae est dolor necessitatibus doloremque amet voluptatem. Saepe fuga error aut facere maiores ipsum consequuntur est officiis tempora libero perspiciatis dolorum placeat, repudiandae ipsam? Totam, vero, sint ducimus inventore ipsum soluta illum aperiam hic aut nobis similique, veritatis illo autem omnis odio doloremque sequi nisi et incidunt quis tenetur. Voluptate repellendus eligendi sint voluptates, quos possimus dolore sunt facere vero ut placeat ad nostrum officia, corrupti at est temporibus natus cum exercitationem architecto odio tempora quibusdam, necessitatibus recusandae. Id impedit omnis quae blanditiis, quasi tempore nemo. Quos, sapiente delectus? Laudantium alias, aliquid facere soluta laborum eos delectus suscipit. Quam, a! Aperiam deserunt sint laboriosam voluptatum aliquam cumque, illum eius nesciunt ipsa necessitatibus, nulla quisquam, maxime exercitationem officiis nisi excepturi? Quis beatae veritatis at molestiae pariatur maxime perferendis voluptatem non vel! Accusamus, vel aliquid harum consectetur eligendi nemo corrupti saepe nulla, temporibus nisi, omnis voluptates laudantium veniam expedita asperiores illo magnam aliquam atque minus provident pariatur architecto eaque ullam! Exercitationem, vitae. Quasi soluta nobis porro ipsa molestias facilis sit est voluptatem suscipit inventore unde ab eos voluptate nihil ipsam laborum voluptas nemo, omnis qui. Culpa!
+                        </h1>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+};
+
+export default About;
