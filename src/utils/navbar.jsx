c43fc201325ef229f6a5266206f6c00c2d5500b8
samuelsibarani11 <samuelalbi09@gmail.com>
2024-06-08 20:01:42 +0700

    update-navbar-create

--- src/utils/navbar.jsx
+++ src/utils/navbar.jsx
@@ -1,6 +1,6 @@
 // eslint-disable-next-line no-unused-vars
 import React, { useState } from 'react';
-import {Link} from 'react-scroll'
+import { Link } from 'react-scroll'
 const Navbar = () => {
     const [isMenuOpen, setIsMenuOpen] = useState(false);
 
@@ -70,6 +70,14 @@ const Navbar = () => {
                                 className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
                             >
                                 Project
+                            </a>
+                        </li>
+                        <li>
+                            <a
+                                href="#certificate"
+                                className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+                            >
+                                Certificate
                             </a>
                         </li>
                         <li>
